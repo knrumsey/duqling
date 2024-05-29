47820ed4db175a7f518e20e4032745cc8ed0a0a8
Kelin Rumsey <knrumsey@pn2004129.lanl.gov>
2024-05-29 18:23:32 -0500

    Added response_type to data_quack info

---
+++ R/data_quack.R
@@ -0,0 +1,137 @@
+#' Get UQ Data for Emulation
+#'
+#' Load in UQ datasets from the UQdataverse (harvard)
+#'
+#' @param raw Logical. When TRUE, full dataset information is given. Otherwise, returns list of scalar-response datasets for emulation.
+#' @param dname Vector of function names to filter.
+#' @param input_dim Vector of input dimensions (quantitative only) to filter.
+#' @param input_cat_dim Vector of input dimensions (categorical only) to filter.
+#' @param n Vector of sample sizes to filter.
+#' @param output_dim Vector of output dimensions to filter.
+#' @param response_type String. Used only when \code{raw=FALSE}. Can be "uni" for univariate response or "func" for functional response.
+#' @return A table with data set names meeting the filter requirements
+#' @details Wrapper to read in various UQ datasets (from harvard dataverse)
+#' @export
+#' @examples
+#' tab <- data_quack(raw=TRUE)
+#' get_UQ_data(tab$dname[1])
+#'
+#' tab <- data_quack(raw=FALSE)
+#' get_emulation_data(tab$dname[1])
+data_quack <- function(raw=FALSE, dname=NULL, input_dim=NULL, input_cat_dim=NULL, n=NULL, output_dim=NULL, response_type=NULL){
+  if(raw){
+    data_quack_raw(dname, input_dim=input_dim, output_dim=output_dim, n=n, input_cat_dim=input_cat_dim)
+  }else{
+    data_quack_emulator(dname, input_dim=input_dim, n=n, input_cat_dim=input_cat_dim, response_type=response_type)
+  }
+}
+
+data_quack_raw <- function(dname=NULL, input_dim=NULL, output_dim=NULL, n=NULL, input_cat_dim=NULL){
+  tab <- NULL
+
+  tmp <- data.frame(dname="strontium_plume", input_dim=20, output_dim=10, n=300, input_cat_dim=0)
+  tab <- rbind(tab, tmp)
+
+  tmp <- data.frame(dname="pbx9501", input_dim=6, output_dim=10, n=7000, input_cat_dim=1)
+  tab <- rbind(tab, tmp)
+
+  tmp <- data.frame(dname="stochastic_sir", input_dim=4, output_dim=1, n=2000, input_cat_dim=0)
+  tab <- rbind(tab, tmp)
+
+  tmp <- data.frame(dname="e3sm", input_dim=2, output_dim=1, n=48602, input_cat_dim=1)
+  tab <- rbind(tab, tmp)
+
+  tmp <- data.frame(dname="fair_climate", input_dim=46, output_dim=1, n=168168, input_cat_dim=1)
+  tab <- rbind(tab, tmp)
+
+  # Subset the master table
+  ord <- order(tab$input_dim)
+  tab <- tab[ord,]
+  res <- tab
+  if(!is.null(dname)){
+    tmp <- dname
+    res <- subset(res, res$dname %in% tmp)
+  }
+  if(!is.null(input_dim)){
+    tmp <- input_dim
+    res <- subset(res, res$input_dim %in% tmp)
+  }
+  if(!is.null(input_cat_dim)){
+    tmp <- input_cat_dim
+    res <- subset(res, res$input_cat_dim %in% tmp)
+  }
+  if(!is.null(output_dim)){
+    tmp <- output_dim
+    res <- subset(res, res$output_dim %in% tmp)
+  }
+  if(!is.null(n)){
+    tmp <- n
+    res <- subset(res, res$n %in% tmp)
+  }
+  return(res)
+}
+
+data_quack_emulator <- function(dname, input_dim=NULL, input_cat_dim=NULL, n=NULL, response_type=NULL){
+  tab <- NULL
+
+  tmp <- data.frame(dname="strontium_plume_p4b", input_dim=20, input_cat_dim=0, n=300, response_type="uni")
+  tab <- rbind(tab, tmp)
+  tmp <- data.frame(dname="strontium_plume_p104", input_dim=20, input_cat_dim=0, n=300, response_type="uni")
+  tab <- rbind(tab, tmp)
+
+  tmp <- data.frame(dname="pbx9501_gold", input_dim=6, input_cat_dim=0, n=500, response_type="uni")
+  tab <- rbind(tab, tmp)
+  tmp <- data.frame(dname="pbx9501_ss304", input_dim=6, input_cat_dim=0, n=500, response_type="uni")
+  tab <- rbind(tab, tmp)
+  tmp <- data.frame(dname="pbx9501_nickel", input_dim=6, input_cat_dim=0, n=500, response_type="uni")
+  tab <- rbind(tab, tmp)
+  tmp <- data.frame(dname="pbx9501_uranium", input_dim=6, input_cat_dim=0, n=500, response_type="uni")
+  tab <- rbind(tab, tmp)
+
+  tmp <- data.frame(dname="stochastic_sir", input_dim=4, input_cat_dim=0, n=2000, response_type="uni")
+  tab <- rbind(tab, tmp)
+
+  tmp <- data.frame(dname="e3sm", input_dim=2, input_cat_dim=0, n=48602, response_type="uni")
+  tab <- rbind(tab, tmp)
+
+  tmp <- rbind(data.frame(dname="fair_climate_ssp1-2.6", input_dim=46, input_cat_dim=0, n=56056, response_type="uni"),
+               data.frame(dname="fair_climate_ssp2-4.5", input_dim=46, input_cat_dim=0, n=56056, response_type="uni"),
+               data.frame(dname="fair_climate_ssp3-7.0", input_dim=46, input_cat_dim=0, n=56056, response_type="uni"),
+               data.frame(dname="fair_climate_ssp1-2.6_year2200", input_dim=45, input_cat_dim=0, n=1001, response_type="uni"),
+               data.frame(dname="fair_climate_ssp2-4.5_year2200", input_dim=45, input_cat_dim=0, n=1001, response_type="uni"),
+               data.frame(dname="fair_climate_ssp3-7.0_year2200", input_dim=45, input_cat_dim=0, n=1001, response_type="uni")
+  )
+  tab <- rbind(tab, tmp)
+
+  # Subset the master table
+  ord <- order(tab$input_dim)
+  tab <- tab[ord,]
+  res <- tab
+  if(!is.null(dname)){
+    tmp <- dname
+    res <- subset(res, res$dname %in% tmp)
+  }
+  if(!is.null(input_dim)){
+    tmp <- input_dim
+    res <- subset(res, res$input_dim %in% tmp)
+  }
+  if(!is.null(input_cat_dim)){
+    tmp <- input_cat_dim
+    res <- subset(res, res$input_cat_dim %in% tmp)
+  }
+  if(!is.null(n)){
+    tmp <- n
+    res <- subset(res, res$n %in% tmp)
+  }
+  if(!is.null(response_type)){
+    tmp <- response_type
+    res <- subset(res, res$response_type %in% tmp)
+  }
+  return(res)
+}
+
+# Workflow
+# Add data to UQDataverse (harvard)
+# Add data read to get_UQ_data
+# Add wrappers for get_emulation_data
+# Add function info to both data_quack_raw() and data_quack_emulation().
--- R/get_UQ_data.R
+++ R/get_UQ_data.R
@@ -2,7 +2,7 @@
 #'
 #' Load in UQ datasets from the UQdataverse (harvard)
 #'
-#' @param name String. Name of dataset (see documentation)
+#' @param dname String. Name of dataset (see documentation)
 #' @param control A list of control variables (currently not used)
 #' @return Data frame
 #' @details Wrapper to read in various UQ datasets (from harvard dataverse)
@@ -48,7 +48,7 @@ get_UQ_data <- function(dname, control=list()){
 #'
 #' Load in UQ datasets from the UQdataverse (harvard)
 #'
-#' @param name String. Name of dataset (see documentation)
+#' @param dname String. Name of dataset (see documentation)
 #' @return A list with elements `y` (a univariate response) and `X` (a matrix of inputs).
 #' @details Wrapper to read in various UQ datasets (from harvard dataverse)
 #' #' @details Wrapper to read in various UQ datasets (from harvard dataverse)
@@ -153,139 +153,4 @@ get_emulation_data <- function(dname){
   return(FALSE)
 }
 
-
-#' Get UQ data for emulation
-#'
-#' Load in UQ datasets from the UQdataverse (harvard)
-#'
-#' @param raw Logical. When TRUE, full dataset information is given. Otherwise, returns list of scalar-response datasets for emulation.
-#' @param dname Vector of function names to filter.
-#' @param input_dim Vector of input dimensions (quantitative only) to filter.
-#' @param input_cat_dim Vector of input dimensions (categorical only) to filter.
-#' @param n Vector of sample sizes to filter.
-#' @param output_dim Vector of output dimensions to filter.
-#' @return A table with data set names meeting the filter requirements
-#' @details Wrapper to read in various UQ datasets (from harvard dataverse)
-#' @export
-#' @examples
-#' tab <- data_quack(raw=TRUE)
-#' get_UQ_data(tab$dname[1])
-#'
-#' tab <- data_quack(raw=FALSE)
-#' get_emulation_data(tab$dname[1])
-data_quack <- function(raw=FALSE, dname=NULL, input_dim=NULL, input_cat_dim=NULL, n=NULL, output_dim=NULL){
-  if(raw){
-    data_quack_raw(dname)
-  }else{
-    data_quack_emulator(dname)
-  }
-}
-
-data_quack_raw <- function(dname=NULL, input_dim=NULL, output_dim=NULL, n=NULL, input_cat_dim=NULL){
-  tab <- NULL
-
-  tmp <- data.frame(dname="strontium_plume", input_dim=20, output_dim=10, n=300, input_cat_dim=0)
-  tab <- rbind(tab, tmp)
-
-  tmp <- data.frame(dname="pbx9501", input_dim=6, output_dim=10, n=7000, input_cat_dim=1)
-  tab <- rbind(tab, tmp)
-
-  tmp <- data.frame(dname="stochastic_sir", input_dim=4, output_dim=1, n=2000, input_cat_dim=0)
-  tab <- rbind(tab, tmp)
-
-  tmp <- data.frame(dname="e3sm", input_dim=2, output_dim=1, n=48602, input_cat_dim=1)
-  tab <- rbind(tab, tmp)
-
-  tmp <- data.frame(dname="fair_climate", input_dim=46, output_dim=1, n=168168, input_cat_dim=1)
-  tab <- rbind(tab, tmp)
-
-  # Subset the master table
-  ord <- order(tab$input_dim)
-  tab <- tab[ord,]
-  res <- tab
-  if(!is.null(dname)){
-    tmp <- dname
-    res <- subset(res, res$dname %in% tmp)
-  }
-  if(!is.null(input_dim)){
-    tmp <- input_dim
-    res <- subset(res, res$input_dim %in% tmp)
-  }
-  if(!is.null(input_cat_dim)){
-    tmp <- input_cat_dim
-    res <- subset(res, res$input_cat_dim %in% tmp)
-  }
-  if(!is.null(output_dim)){
-    tmp <- output_dim
-    res <- subset(res, res$output_dim %in% tmp)
-  }
-  if(!is.null(n)){
-    tmp <- n
-    res <- subset(res, res$n %in% tmp)
-  }
-  return(res)
-}
-
-data_quack_emulator <- function(dname, input_dim=NULL, input_cat_dim=NULL, n=NULL){
-  tab <- NULL
-
-  tmp <- data.frame(dname="strontium_plume_p4b", input_dim=20, input_cat_dim=0, n=300)
-  tab <- rbind(tab, tmp)
-  tmp <- data.frame(dname="strontium_plume_p104", input_dim=20, input_cat_dim=0, n=300)
-  tab <- rbind(tab, tmp)
-
-  tmp <- data.frame(dname="pbx9501_gold", input_dim=6, input_cat_dim=0, n=500)
-  tab <- rbind(tab, tmp)
-  tmp <- data.frame(dname="pbx9501_ss304", input_dim=6, input_cat_dim=0, n=500)
-  tab <- rbind(tab, tmp)
-  tmp <- data.frame(dname="pbx9501_nickel", input_dim=6, input_cat_dim=0, n=500)
-  tab <- rbind(tab, tmp)
-  tmp <- data.frame(dname="pbx9501_uranium", input_dim=6, input_cat_dim=0, n=500)
-  tab <- rbind(tab, tmp)
-
-  tmp <- data.frame(dname="stochastic_sir", input_dim=4, input_cat_dim=0, n=2000)
-  tab <- rbind(tab, tmp)
-
-  tmp <- data.frame(dname="e3sm", input_dim=2, input_cat_dim=0, n=48602)
-  tab <- rbind(tab, tmp)
-
-  tmp <- rbind(data.frame(dname="fair_climate_ssp1-2.6", input_dim=46, input_cat_dim=0, n=56056),
-            data.frame(dname="fair_climate_ssp2-4.5", input_dim=46, input_cat_dim=0, n=56056),
-            data.frame(dname="fair_climate_ssp3-7.0", input_dim=46, input_cat_dim=0, n=56056),
-            data.frame(dname="fair_climate_ssp1-2.6_year2200", input_dim=45, input_cat_dim=0, n=1001),
-            data.frame(dname="fair_climate_ssp2-4.5_year2200", input_dim=45, input_cat_dim=0, n=1001),
-            data.frame(dname="fair_climate_ssp3-7.0_year2200", input_dim=45, input_cat_dim=0, n=1001)
-          )
-  tab <- rbind(tab, tmp)
-
-  # Subset the master table
-  ord <- order(tab$input_dim)
-  tab <- tab[ord,]
-  res <- tab
-  if(!is.null(dname)){
-    tmp <- dname
-    res <- subset(res, res$dname %in% tmp)
-  }
-  if(!is.null(input_dim)){
-    tmp <- input_dim
-    res <- subset(res, res$input_dim %in% tmp)
-  }
-  if(!is.null(input_cat_dim)){
-    tmp <- input_cat_dim
-    res <- subset(res, res$input_cat_dim %in% tmp)
-  }
-  if(!is.null(n)){
-    tmp <- n
-    res <- subset(res, res$n %in% tmp)
-  }
-  return(res)
-}
-
-
-
-# Workflow
-# Add data to UQDataverse (harvard)
-# Add data read to get_UQ_data
-# Add wrappers for get_emulation_data
-# Add function info to both data_quack_raw() and data_quack_emulation().
 
